9868e18d5ac604d51d77d02fc24e13aae0daf5d3
Fabrício Pereira <85375809+WarrenCollins473@users.noreply.github.com>
2023-10-10 16:42:11 -0300

    feat/first-page-elements-1.2

--- src/screens/profile/index.tsx
+++ src/screens/profile/index.tsx
@@ -1,29 +1,48 @@
 import React from "react";
-import { View, Text, StyleSheet, Image, Switch } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  Switch,
+  TouchableOpacity,
+} from "react-native";
 import { ProfileText } from "./styles";
+import { Dimensions } from "react-native";
 
+var width = Dimensions.get("window").width;
 export function Profile() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>My profile</Text>
+      <View style={styles.titleConteiner}>
+        <Text style={styles.title}>My profile</Text>
+      </View>
       <View>
         <Image
           style={styles.image}
           source={require("@screens/profile/image.png")}
         ></Image>
+      </View>
+      <View>
         {/* <ProfileText>Juliane Golçalves Freitas</ProfileText> */}
         <Text style={styles.profileName}>Juliane Golçalves Freitas</Text>
         <Text style={styles.profileEmail}>matildabrown@mail.com</Text>
       </View>
-      <View style={styles.profileOptions}>
-        <Text style={styles.profileOptionsText}>Edit Informations</Text>
-        <Switch></Switch>
-      </View>
-      <View style={styles.profileOptions}>
-        <Text style={styles.profileOptionsText}>Language</Text>
-      </View>
-      <View style={styles.profileOptions}>
-        <Text style={styles.profileOptionsText}>Log out</Text>
+      <View style={styles.conteinerOptions}>
+        <View style={styles.profileOptions}>
+          <Text style={styles.profileOptionsText}>Edit Informations</Text>
+          <Switch></Switch>
+        </View>
+        <TouchableOpacity>
+          <View style={styles.profileOptions}>
+            <Text style={styles.profileOptionsText}>Language</Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <View style={styles.profileOptions}>
+            <Text style={styles.profileOptionsText}>Log out</Text>
+          </View>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -39,6 +58,8 @@ const styles = StyleSheet.create({
   title: {
     fontWeight: "800",
     fontSize: 32,
+    alignItems: "flex-start",
+    marginHorizontal: 20,
   },
   image: {
     borderRadius: 100,
@@ -58,8 +79,20 @@ const styles = StyleSheet.create({
     fontSize: 16,
   },
   profileOptions: {
+    justifyContent: "space-between",
+    alignItems: "center",
     flexDirection: "row",
     borderBottomColor: "black",
     borderBottomWidth: 1,
+    height: 73,
+    padding: 20,
+    margin: 1,
+  },
+  conteinerOptions: {
+    width: width,
+  },
+  titleConteiner: {
+    alignItems: "flex-start",
+    width: width,
   },
 });

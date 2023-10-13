4ebd82f75dbb3d56586d8b4295696d6707cf2d5a
Pedro Fernandes Lopes <pedroflopes1710@gmail.com>
2023-10-13 12:01:38 -0300

    chore: general structure on the home page

--- App.tsx
+++ App.tsx
@@ -18,17 +18,17 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* <Text>Open up App.tsx to start working on your app!</Text> */}
+      {/* <StatusBar style="auto" /> */}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
+    // flex: 1,
+    // backgroundColor: "#fff",
+    // alignItems: "center",
+    // justifyContent: "center"
   }
 });
---
+++ src/screens/MainPage/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { ScrollView, StatusBar} from 'react-native';
+
+import HeaderMain from '../../components/HeaderMain';
+
+
+function MainPage() {
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#000" />
+      <ScrollView>
+        <HeaderMain />
+      </ScrollView>
+    </>
+  );
+}
